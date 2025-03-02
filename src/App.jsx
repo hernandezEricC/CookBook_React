baaa94d794e65d306b414f51e28dc154ddfac136
Eric <60497890+eRicCharlEs@users.noreply.github.com>
2025-03-02 11:45:58 -0800

    Minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+/**
+ * @description The main App component that renders the application
+ * It contains a counter and links to Vite and React documentation
+ * @returns {JSX.Element}
+ */
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const copyrightYear = new Date().getFullYear();
 
   return (
     <>
@@ -12,22 +18,31 @@ function App() {
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
+        
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+      
       </div>
-      <h1>Vite + React</h1>
+      
+      <h1>My Cook Book</h1>
+      
       <div className="card">
+        
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
+      
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      
+      <p className="copyright">
+        Copyright © {copyrightYear} <a href="https://github.com/hernandezEricC">Eric Hernandez</a>. All rights reserved.
       </p>
+    
     </>
   )
 }
